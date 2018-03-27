eb18b2dc1c65b7ee30afa5eb9e8c6864f46c47ba
lwesy <lw@esyoil.com>
2018-03-27 08:42:48 +0200

    prevent xss attacks

--- init.php
+++ init.php
@@ -1,6 +1,10 @@
 <?php
 
 require __DIR__ . "./autoload.php";
-require __DIR__ . "./database.php";
+
+function e($str)
+{
+    return htmlentities($str, ENT_QUOTES, "UTF-8");
+}
 
 $container = new App\Core\Container();
